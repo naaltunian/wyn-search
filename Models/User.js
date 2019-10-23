863ab5136063676467e15adf166a902c390c541b
Nick <naaltunian@icloud.com>
2019-10-22 22:38:01 -0400

    Update User model

--- Models/User.js
+++ Models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
     personalSite: String,
+    bio: {
+        type: String,
+        required: true
+    },
     dateCreated: {
         type: Date,
         default: Date.now
