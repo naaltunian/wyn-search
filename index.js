14a0118bfe6ae6c06800b651699b8c4e1366096b
Nick <naaltunian@icloud.com>
2019-10-22 21:59:21 -0400

    Add database connection

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 // const { ApolloServer } = require('apollo-server-express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+// database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 
