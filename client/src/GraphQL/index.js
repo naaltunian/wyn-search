acbc1265f9e8c5ac5bf40ba38682932ea190a6f5
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-09 13:54:32 -0500

    added login mutation

--- client/src/GraphQL/index.js
+++ client/src/GraphQL/index.js
@@ -6,6 +6,11 @@ mutation createUser($userInput: UserInput!) {
   createUser(userInput: $userInput)
 }`;
 
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!){
+  login(email: $email, password: $password)
+}`;
+
 export const GET_ALL_USERS = gql`
 query getAllUsers {
     name
