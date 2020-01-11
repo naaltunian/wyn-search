671d599bba18608a50118cc92b9bc8e56a89f623
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-11 17:41:53 -0500

    created student component for student page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import reducer from './reducer';
 import Navbar from './Components/Navbar';
 import Profile from './Components/Profile/Profile';
 import Home from './Components/Students/Home';
+import Student from './Components/Student/Student'
 import SignUp from './Components/Authentication/SignUp';
 import Login from './Components/Authentication/Login';
 import UserContext from './Contexts/UserContext';
@@ -11,14 +12,14 @@ import UserContext from './Contexts/UserContext';
 const App = (props) => {
   const initialState = useContext(UserContext);
   const [state, dispatch] = useReducer(reducer, initialState);
-  
+
   // keeps userContext authorized if signed in
   useEffect(_ => {
     if (localStorage.getItem("token") != null) {
       dispatch({type: "LOGIN"});
     }
   }, [initialState.isAuth]);
-  
+
   return (
     <BrowserRouter>
       <>
@@ -26,6 +27,7 @@ const App = (props) => {
           <Navbar isAuth={state.isAuth} client={props.client} />
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/user/:id" component={Student}/>
               <Route path="/signup" component={SignUp} />
               <Route path="/profile" component={Profile} />
               <Route path="/login" component={Login} />
--- client/src/Components/Student/Student.js
+++ client/src/Components/Student/Student.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { GET_USER } from '../../GraphQL/index';
+import { useQuery } from '@apollo/react-hooks';
 
-const Student = ({ name, email, bio, github, skills, site}) => {
-    return (
-        <div style={{
-          border: 'solid 1px red',
-          width: '200px'
-        }}>
-            <h1>{name}</h1>
-            <p>{email}</p>
-            <p>{github}</p>
-            <p>{site}</p>
-            <p>{skills}</p>
-            <p>{bio}</p>
+const Student = (props) => {
+    const id = props.location.state.id
+    const { loading, error, data } = useQuery(GET_USER, { variables: { _id: id } });
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    const user = data.getUser
+    return(
+        <div>
+            <h1>{user.name}</h1>
+            <p>{user.email}</p>
+            <p>{user.githubUsername}</p>
+            <p>{user.personalSite}</p>
+            <p>{user.bio}</p>
+            <ul>
+            {user.skills.map( skill => (
+                <li>{skill}</li>
+            ))}
+            </ul>
+
         </div>
-    );
+    )
 }
 
 export default Student;
