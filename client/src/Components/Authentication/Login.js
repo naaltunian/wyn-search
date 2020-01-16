ce10399891f81cc01583d3e4a5641e919ab3785c
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-16 17:50:31 -0500

    styles on login page

--- client/src/Components/Authentication/Login.js
+++ client/src/Components/Authentication/Login.js
@@ -1,47 +1,153 @@
-import React, { useState, useContext, useReducer } from 'react';
-import UserContext from '../../Contexts/UserContext';
-import Container from '@material-ui/core/Container';
-import { LOGIN } from '../../GraphQL/index';
-import { useMutation } from '@apollo/react-hooks';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useContext, useReducer } from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+
+import UserContext from '../../Contexts/UserContext'
+import { LOGIN } from '../../GraphQL/index'
+import { useMutation } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://wyncode.co/">
+        Wyncode
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
 
 const INITIAL_STATE = {
-    email: "",
-    password: "",
+  email: '',
+  password: ''
 }
 
 const Login = ({ isAuth }) => {
-    const message = useContext(UserContext)
+  const classes = useStyles()
+  const message = useContext(UserContext)
 
-    const [user, setUser] = useState(INITIAL_STATE);
-    const [login, { data, error }] = useMutation(LOGIN, { variables: { email: user.email, password: user.password } });
-    const { dispatch } = useContext(UserContext);
-    let history = useHistory();
+  const [user, setUser] = useState(INITIAL_STATE)
+  const [login, { data, error }] = useMutation(LOGIN, {
+    variables: { email: user.email, password: user.password }
+  })
+  const { dispatch } = useContext(UserContext)
+  let history = useHistory()
 
-    const handleInputChange = field => e => setUser({ ...user, [field]: e.target.value });
+  const handleInputChange = field => e =>
+    setUser({ ...user, [field]: e.target.value })
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        let { data } = await login();
-        await localStorage.setItem('token', data.login);
-        dispatch({type: "LOGIN"});
-        history.push('/');
-    };
+  const handleSubmit = async e => {
+    e.preventDefault()
+    let { data } = await login()
+    await localStorage.setItem('token', data.login)
+    dispatch({ type: 'LOGIN' })
+    history.push('/')
+  }
 
-    const isInvalid = !user.email || !user.password;
+  const isInvalid = !user.email || !user.password
 
-    return(
-        <>
-            <div>
-                {isAuth}
-            </div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="email" value={user.email} placeholder="email" onChange={handleInputChange("email")} />
-                <input type="password" name="password" value={user.password} placeholder="password" onChange={handleInputChange("password")} />
-                <input type="submit" value="Submit" disabled={isInvalid} />
-            </form>
-        </>
-    )
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit} className={classes.form} noValidat>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={user.email}
+            placeholder="email"
+            onChange={handleInputChange('email')}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={user.password}
+            placeholder="password"
+            onChange={handleInputChange('password')}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={isInvalid}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
 }
 
-export default Login;
+export default Login
