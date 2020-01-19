04d34b35dedb805e89cda0c74ce8ba9fa4f5853f
Nick <naaltunian@icloud.com>
2020-01-19 08:19:17 -0500

    Add updated profile modal
    When updating a user's profile, the user will see a modal stating the profile was successfully updated

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -7,11 +7,12 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 
 const Profile = () => {
     const { loading, error, data } = useQuery(GET_CURRENT_USER);
@@ -27,6 +28,7 @@ const Profile = () => {
     const classes = useFormStyles();
     const [user, setUser] = useState(INITIAL_STATE);
     const [userId, setId] = useState("");
+    const [open, setOpen] = React.useState(false);
     const [updateUser, {data: mutationData}] = useMutation(UPDATE_USER, { variables: { _id: userId, userInput: user }, refetchQueries: [{query: GET_CURRENT_USER}] });
 
     useEffect(_ => {
@@ -47,7 +49,16 @@ const Profile = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         let { data } = await updateUser();
+        handleClickOpen();
         history.push("/profile");
+    };
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+    setOpen(false);
     };
 
     return(
@@ -130,6 +141,23 @@ const Profile = () => {
                     Update Profile
                 </Button>
             </form>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Profile Updated
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Container>
     )
 }
